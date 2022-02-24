5b41c513a4927b0600d942137ef0d0890a9fa6f8
Vinicius Souza <vinicius.souza721@hotmail.com>
2022-02-24 17:34:09 -0300

    Update

--- src/app/photos/photo/photos.module.ts
+++ src/app/photos/photo/photos.module.ts
@@ -2,7 +2,7 @@ import { PhotoComponent } from './photo.component';
 import { NgModule } from "@angular/core";
 
 @NgModule({
-  declarations: [ PhotoComponent, ],
+  declarations: [ PhotoComponent ],
   exports: [ PhotoComponent ],
 })
 export class PhotosModule {}
