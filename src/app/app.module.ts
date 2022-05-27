1896b7045d62406faceac5761be74c0a2be3f708
Vinicius Souza <vinicius.souza721@hotmail.com>
2022-05-27 17:09:16 -0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { PhotosModule } from './photos/card/card.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
